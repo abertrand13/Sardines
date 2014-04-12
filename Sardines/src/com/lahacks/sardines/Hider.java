9c43c774d492f100c0e13db258c6bdbc56bf4db6
Thomas Teisberg <teisberg@stanford.edu>
2014-04-12 12:56:36 -0700

    Starting work on the Hider Activity

--- Sardines/src/com/lahacks/sardines/Hider.java
+++ Sardines/src/com/lahacks/sardines/Hider.java
@@ -2,8 +2,13 @@ package com.lahacks.sardines;
 
 import java.util.Locale;
 
+import com.lahacks.sardines.Seeker.DummySectionFragment;
+import com.lahacks.sardines.Seeker.NavigationFragment;
+import com.lahacks.sardines.Seeker.StreamFragment;
+
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,6 +16,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -133,14 +139,23 @@ public class Hider extends FragmentActivity implements ActionBar.TabListener {
 
 		@Override
 		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
+			Fragment f;
+			
+			switch(position){
+			case 1:
+				f = new StreamFragment();
+				break;
+			case 2:
+				f = new PlayersFragment();
+				break;
+			default:
+				f = new NavigationHiderFragment();
+				break;
+			}
 			Bundle args = new Bundle();
 			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
-			return fragment;
+			f.setArguments(args);
+			return f;
 		}
 
 		@Override
@@ -154,39 +169,30 @@ public class Hider extends FragmentActivity implements ActionBar.TabListener {
 			Locale l = Locale.getDefault();
 			switch (position) {
 			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
+				return getString(R.string.title_section_navigation).toUpperCase(l);
 			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
+				return getString(R.string.title_section_stream).toUpperCase(l);
 			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
+				return getString(R.string.title_section_players).toUpperCase(l);
 			}
 			return null;
 		}
 	}
+	
+	public static class NavigationHiderFragment extends NavigationFragment{
+		// TODO
+	}
+	
+	public static class PlayersFragment extends Fragment {
 
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
+		public PlayersFragment() {
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_hider_dummy,
+			View rootView = inflater.inflate(R.layout.fragment_hider_players,
 					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
 			return rootView;
 		}
 	}
