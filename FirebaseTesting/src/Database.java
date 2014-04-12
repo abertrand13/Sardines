f5d5a9e9111a27a3472d98d11ab9a991b9bf3e63
Alex Bertrand <alex.bertrand13@gmail.com>
2014-04-12 08:43:25 -0700

    WHY ARE THE COMMENTS RUNNING

--- FirebaseTesting/src/Database.java
+++ FirebaseTesting/src/Database.java
@@ -1,4 +1,6 @@
 import com.firebase.client.*;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Database {
 	
@@ -16,14 +18,72 @@ public class Database {
 		database.child("String1").setValue("This is String1");
 		database.child("String2").setValue("This is String2");
 		
-		/*database.addValueEventListener(new ValueEventListener() {
+		Map<String, Object> mappy = new HashMap<String, Object>();
+		mappy.put("Map from one thing", "to another!");
+		mappy.put("Vadim", "Khayms");
+		database.child("Map").setValue(mappy);
+		
+		database.child("How").child("far").child("can").child("I").child("go?").setValue("Far!");
+		
+		//SAMPLE PLAYER DATABASE
+		Firebase gameRef = database.child("Game ID UE45H8A");
+		gameRef.setValue("SHITZ");
+		
+		Firebase game2Ref = database.child("Game ID R45THG8");
+		game2Ref.setValue("MOAR SHITZ");
+		
+		game2Ref.child("player1").setValue("Test.");
+		game2Ref.child("player1").child("name").setValue("LIL JON");
+		game2Ref.child("player1").child("lat").setValue("2");
+		game2Ref.child("player1").child("long").setValue("3");
+		game2Ref.child("player1").child("ready").setValue("FUCK YEA");
+		
+		
+		//READING DATA (CONSTANTLY)
+		database.addValueEventListener(new ValueEventListener() {
 
 		    @Override
 		    public void onDataChange(DataSnapshot snap) {
 		        System.out.println(snap.getName() + " -> " + snap.getValue());
 		    }
 
-		    @Override public void onCancelled() { }
-		});*/
+		    @Override
+		    public void onCancelled() {
+		    	System.out.println("Listener was cancelled");
+		    }
+		});
+		
+		database.addChildEventListener(new ChildEventListener() {
+		    @Override
+		    public void onChildAdded(DataSnapshot snapshot, String previousChildName) {
+		        //String userName = snapshot.getName();
+		        //GenericTypeIndicator<Map<String, Object>> t = new GenericTypeIndicator<Map<String, Object>>() {};
+		        //Map<String, Object> userData = snapshot.getValue(t);
+		        //System.out.println("User " + userName + " has entered the chat");
+		    	//System.out.println("woop woop!");
+		    }
+
+		    @Override
+		    public void onChildChanged(DataSnapshot snapshot, String previousChildName) {
+
+		    }
+
+		    @Override
+		    public void onChildRemoved(DataSnapshot snapshot) {
+
+		    }
+
+		    @Override
+		    public void onChildMoved(DataSnapshot snapshot, String previousChildName) {
+
+		    }
+
+		    @Override
+		    public void onCancelled() {
+
+		    }
+		});
+		
+		
 	}
 }
