be35c536f42106c2bbb687a0de1e5bcd2ced79ec
Thomas Teisberg <teisberg@stanford.edu>
2014-04-12 06:25:25 -0700

    Compass thread groundwork

--- Sardines/src/com/lahacks/sardines/CompassView.java
+++ Sardines/src/com/lahacks/sardines/CompassView.java
@@ -6,45 +6,109 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class CompassView extends View {
+public class CompassView extends SurfaceView implements SurfaceHolder.Callback {
 
-	Context context;
+	private Context context;
+	private CompassViewThread thread;
+	
+	private double angle = 90;
+	private double arcRange = 30;
+	private double targetAngle = 180;
+	private double targetArcRange = 30;
 
 	public CompassView(Context context) {
 		super(context);
 		this.context = context;
+		setup();
 
 	}
 
 	public CompassView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context = context;
+		setup();
 	}
 
 	public CompassView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		this.context = context;
+		setup();
+	}
+
+	private void setup() {
+	}
+
+	public void setAngle(double a, double range){
+		angle = a;
+		arcRange = range;
+	}
+	
+	public void setAngleTarget(double a, double range){
+		targetAngle = a;
+		targetArcRange = range;
+	}
+	
+	public double getAngle() {
+		return angle;
+	}
+
+	public double getRange() {
+		return arcRange;
+	}
+
+	public double getTargetAngle() {
+		return targetAngle;
+	}
+
+	public double getTargetRange() {
+		return targetArcRange;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		int angle = 90;
-		int arcRange = 45;
-		draw(canvas, angle, arcRange);
+		draw(canvas, (int)angle, (int)arcRange);
 	}
 
-	private void draw(Canvas canvas, int angle, int arcRange){
+	private void draw(Canvas canvas, int angle, int arcRange) {
 		int x = getWidth();
 		int y = getHeight();
 		int r = Math.min(x, y);
 		Paint paint = new Paint();
 		paint.setStyle(Paint.Style.FILL);
 		paint.setColor(Color.WHITE);
-		int drawAngle = angle - (arcRange/2);
-		if(drawAngle < 0) drawAngle += 360;
+		int drawAngle = angle - (arcRange / 2);
+		if (drawAngle < 0)
+			drawAngle += 360;
 		canvas.drawArc(new RectF(0, 0, r, r), drawAngle, arcRange, true, paint);
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+
+		setWillNotDraw(false); // Allows us to use invalidate() to call onDraw()
+
+		thread = new CompassViewThread(getHolder(), this); // Start the thread that
+		//thread.setRunning(true); // will make calls to
+		//thread.start(); // onDraw()
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		try {
+			thread.setRunning(false); // Tells thread to stop
+			thread.join(); // Removes thread from mem.
+		} catch (InterruptedException e) {
+		}
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- Sardines/src/com/lahacks/sardines/CompassViewThread.java
+++ Sardines/src/com/lahacks/sardines/CompassViewThread.java
@@ -4,46 +4,58 @@ import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
 class CompassViewThread extends Thread {
-        private SurfaceHolder _surfaceHolder;
-        private CompassView _compass;
-        private boolean _run = false;
+	private SurfaceHolder _surfaceHolder;
+	private CompassView compass;
+	private boolean _run = false;
 
+	private double dpsAngle = 10;
+	private double dpsRange = 10;
+	
+	private long lastTime = 0;
 
-        public CompassViewThread(SurfaceHolder surfaceHolder, CompassView cv) {
-            _surfaceHolder = surfaceHolder;
-            _compass = cv;
-        }
+	public CompassViewThread(SurfaceHolder surfaceHolder, CompassView cv) {
+		_surfaceHolder = surfaceHolder;
+		compass = cv;
+	}
 
+	public void setRunning(boolean run) { // Allow us to stop the thread
+		_run = run;
+	}
 
-        public void setRunning(boolean run) { //Allow us to stop the thread
-            _run = run;
-        }
+	@Override
+	public void run() {
+		Canvas c;
+		while (_run) { // When setRunning(false) occurs, _run is
+			c = null; // set to false and loop ends, stopping thread
 
+			try {
 
-        @Override
-        public void run() {
-            Canvas c;
-            while (_run) {     //When setRunning(false) occurs, _run is 
-                c = null;      //set to false and loop ends, stopping thread
+				c = _surfaceHolder.lockCanvas(null);
+				synchronized (_surfaceHolder) {
+					int elapsedTime = (int) (System.currentTimeMillis() - lastTime);
+					lastTime = System.currentTimeMillis();
+					
+					double maxChangeAngle = ((double)elapsedTime / 1000.0) * dpsAngle;
+					double maxChangeRange = ((double)elapsedTime / 1000.0) * dpsRange;
+					
+					double newRange = compass.getTargetRange();
+					double newAngle = compass.getTargetAngle();
+					
+					if(newAngle - compass.getAngle() > maxChangeAngle){
+						newAngle = compass.getAngle() + maxChangeAngle;
+					}
+					
+					compass.setAngle(newAngle, newRange);
+					
+					// Insert methods to modify positions of items in onDraw()
+					compass.postInvalidate();
 
-
-                try {
-
-
-                    c = _surfaceHolder.lockCanvas(null);
-                    synchronized (_surfaceHolder) {
-
-
-                     //Insert methods to modify positions of items in onDraw()
-                     _compass.postInvalidate();
-
-
-                    }
-  } finally {
-                    if (c != null) {
-                        _surfaceHolder.unlockCanvasAndPost(c);
-                    }
-                }
-            }
-        }
-    }
+				}
+			} finally {
+				if (c != null) {
+					_surfaceHolder.unlockCanvasAndPost(c);
+				}
+			}
+		}
+	}
+}
