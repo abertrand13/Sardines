b01d2977ca1ae8c8c8952728160a0df809b22912
Thomas Teisberg <teisberg@stanford.edu>
2014-04-12 05:54:14 -0700

    Tweaking compass

--- Sardines/src/com/lahacks/sardines/CompassView.java
+++ Sardines/src/com/lahacks/sardines/CompassView.java
@@ -4,46 +4,47 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class CompassView extends View {
-	
+
 	Context context;
-	
-	public CompassView(Context context)
-	{
-	    super(context);
-	    this.context = context;
+
+	public CompassView(Context context) {
+		super(context);
+		this.context = context;
 
 	}
 
-	public CompassView(Context context, AttributeSet attrs)
-	{
-	    super(context, attrs);
-	    this.context = context;
+	public CompassView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		this.context = context;
 	}
 
-	public CompassView(Context context, AttributeSet attrs, int defStyle)
-	{
-	    super(context, attrs, defStyle);
-	    this.context = context;
+	public CompassView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		this.context = context;
 	}
 
-   @Override
-   protected void onDraw(Canvas canvas) {
-      // TODO Auto-generated method stub
-      super.onDraw(canvas);
-      int x = getWidth();
-      int y = getHeight();
-      int radius;
-      radius = 100;
-      Paint paint = new Paint();
-      paint.setStyle(Paint.Style.FILL);
-      paint.setColor(Color.WHITE);
-      canvas.drawPaint(paint);
-      // Use Color.parseColor to define HTML colors
-      paint.setColor(Color.parseColor("#CD5C5C"));
-      canvas.drawCircle(x / 2, y / 2, radius, paint);
-  }
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		int angle = 90;
+		int arcRange = 45;
+		draw(canvas, angle, arcRange);
+	}
+
+	private void draw(Canvas canvas, int angle, int arcRange){
+		int x = getWidth();
+		int y = getHeight();
+		int r = Math.min(x, y);
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(Color.WHITE);
+		int drawAngle = angle - (arcRange/2);
+		if(drawAngle < 0) drawAngle += 360;
+		canvas.drawArc(new RectF(0, 0, r, r), drawAngle, arcRange, true, paint);
+	}
 }
